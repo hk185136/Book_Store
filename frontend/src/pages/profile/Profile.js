1a38ef71f45a82b7346cd8ab9e091ba9a4b36fd0
Karuparthi <hk185136@ncr.com>
2024-03-21 12:28:59 +0530

    front-end-commit

---
+++ frontend/src/pages/profile/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Orders from '../../components/orders/Orders'
+function Profile() {
+  return (
+    <div>
+        <h1>User name</h1>
+        <h2>Your orders</h2>
+        <Orders/>
+
+    </div>
+  )
+}
+
+export default Profile
